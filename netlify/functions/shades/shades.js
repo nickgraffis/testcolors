057a79b5620c89e457a36cb5031c9f80f97fb3c0
Nick Graffis <nicholasgraffis@gmail.com>
2022-04-28 21:28:20 -0700

    cors

--- netlify/functions/shades/shades.js
+++ netlify/functions/shades/shades.js
@@ -164,6 +164,12 @@ exports.handler = async (event, context) => {
   var shades = shade(RGBToHex(color[0], color[1], color[2]), 1.771968374684816, 7.3903743315508015)
   return {
     statusCode: 200,
+    headers: {
+      /* Required for CORS support to work */
+      'Access-Control-Allow-Origin': '*',
+      /* Required for cookies, authorization headers with HTTPS */
+      'Access-Control-Allow-Credentials': true
+    },
     body: JSON.stringify(shades)
   };
 };
